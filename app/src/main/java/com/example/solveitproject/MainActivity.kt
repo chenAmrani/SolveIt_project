8230d8ac48f9b83bef8b2fafdce83632ad64fd45
chenAmrani <chenamrani5@gmail.com>
2024-03-25 14:02:02 +0200

    add Logs to the main activity

--- app/src/main/java/com/example/solveitproject/MainActivity.kt
+++ app/src/main/java/com/example/solveitproject/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.solveitproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("DEMO3" , "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("DEMO3" , "onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("DEMO3" , "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("DEMO3" , "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("DEMO3" , "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("DEMO3" , "onDestroy")
     }
 }
