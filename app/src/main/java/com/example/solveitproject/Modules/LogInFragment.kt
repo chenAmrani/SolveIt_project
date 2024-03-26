b6b85e62e412c553fc78e3d4469cc6f24b8788ac
chenAmrani <chenamrani5@gmail.com>
2024-03-26 12:07:10 +0200

    update gradle

--- app/src/main/java/com/example/solveitproject/Modules/LogInFragment.kt
+++ app/src/main/java/com/example/solveitproject/Modules/LogInFragment.kt
@@ -1,57 +1,49 @@
 package com.example.solveitproject.Modules
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.solveitproject.R
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.navigation.Navigation
+import com.example.solveitproject.R
 
 
-class RegisterFragment : Fragment() {
+class LogInFragment : Fragment() {
 
-    private var nameTextField: EditText? = null
-    private var idTextField: EditText? = null
     private var emailTextField: EditText? = null
     private var passwordTextField: EditText? = null
-    private var saveButton: Button? = null
-    private var cancelButton: Button? = null
-    private var uploadImageButton: Button? = null
+    private var signInButton: Button? = null
+    private var registerButton: Button? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_log_in, container, false)
         setupUI(view)
+        Log.i("tag","Loading the page problem")
         return view
     }
 
     private fun setupUI(view : View) {
-        nameTextField = view.findViewById(R.id.nameTextField)
-        idTextField = view.findViewById(R.id.idTextField)
         emailTextField = view.findViewById(R.id.emailTextField)
         passwordTextField = view.findViewById(R.id.passwordTextField)
-        saveButton = view.findViewById(R.id.BtnRegisterDone)
-        cancelButton = view.findViewById(R.id.BtnRegisterCancel)
-        uploadImageButton = view.findViewById(R.id.BtnUploadImage)
+        signInButton = view.findViewById(R.id.BtnSignIn)
+        registerButton = view.findViewById(R.id.BtnRegisterLogInFragment)
+        val action = Navigation.createNavigateOnClickListener(R.id.action_logInFragment2_to_registerFragment)
+        registerButton?.setOnClickListener (action)
 
-
-        cancelButton?.setOnClickListener{
-//            finish()  //TODO define later
-        }
-
-        saveButton?.setOnClickListener{
-            val name = nameTextField?.text.toString()
-            val id = idTextField?.text.toString()
+        signInButton?.setOnClickListener{
             val email = emailTextField?.text.toString().trim()
             val password = passwordTextField?.text.toString().trim()
 
-            if (name.isEmpty() || id.isEmpty() || email.isEmpty() || password.isEmpty() ) {
+            if (email.isEmpty() || password.isEmpty() ) {
                 Toast.makeText(
                     requireContext(),
                     "Please fill in all fields",
@@ -59,11 +51,11 @@ class RegisterFragment : Fragment() {
                 ).show()
 //                user.delete()
             }
-
         }
-
     }
 }
+
+
 
 
 
